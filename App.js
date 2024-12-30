9a908454bdb46228013cf24e1cfc0844f98cb777
luisfelipe2104 <gomesluisfelipe05@gmail.com>
2024-12-30 17:28:54 -0300

    adding the circular progress indicator

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect, useContext } from 'react'
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 export default function App() {
   const [count, setCount] = useState(0)
-  const [minutes, setMinutes] = useState(25)
+  const [defaultWorkMinutes, setDefaultWorkMinutes] = useState(25)
+  const [defaultPauseMinutes, setDefaultPauseMinutes] = useState(5)
+  const [minutes, setMinutes] = useState(defaultWorkMinutes)
   const [seconds, setSeconds] = useState(0)
-  const [time, setTime] = useState("25:00")
-  const [isFlowing, setIsFlowing] = useState(true)
+  const [timePercent, setTimePercent] = useState(100)
+  const [time, setTime] = useState(`${defaultWorkMinutes}:00`)
+  const [isPaused, setIsPaused] = useState(false)
   const [isStopped, setIsStopped] = useState(true)
 
   const timeIsOver = () => {
-    setIsFlowing(!isFlowing)
-    if (isFlowing) {
-      setMinutes(25)
+    // gambiarra
+    setIsPaused(!isPaused)
+    if (isPaused) {
+      setMinutes(defaultWorkMinutes)
     }
-    else if (!isFlowing) {
-      setMinutes(5)
+    else if (!isPaused) {
+      setMinutes(defaultPauseMinutes)
     }
+
   }
 
   const formatTime = () => {
     const result = `${minutes < 10 ? "0" + minutes : minutes }:${seconds < 10 ? "0" + seconds : seconds}`
     setTime(result)
+    const workMinutesPercent = (((minutes * 60) + seconds) * 100) / (isPaused === false ? 60 * defaultWorkMinutes : 60 * defaultPauseMinutes)
+    setTimePercent(Math.floor(workMinutesPercent))
     return result
   }
 
@@ -49,6 +57,8 @@ export default function App() {
         setCount(count + 1)
       }, 1000)
       console.log(count);
+      console.log(isPaused);
+      console.log(timePercent);
       tick()
       formatTime()
       return () => clearInterval(intervalId);
@@ -57,9 +67,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => buttonPress()} style={styles.button}>
-        <Text>{isStopped ? "Start" : "Pause"}</Text>
-        <Text>{time}</Text>
+        <CircularProgress
+          value={timePercent}
+          showProgressValue={false}
+          subtitle={isStopped === true ? "Start" : "Pause"}
+          subtitleStyle={styles.subtitle}
+          title={time}
+          titleStyle={styles.title}
+          inActiveStrokeColor={isStopped ? 'red' : '#2ecc71'}
+          inActiveStrokeOpacity={0.2}
+          activeStrokeColor={isStopped ? 'red': '#2ecc71'}
+        />
       </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -73,6 +93,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   button: {
-    backgroundColor: 'green'
-  }
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 20,
+    color: 'green',
+    display: 'flex',
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: '#2ecc71',
+    fontSize: 15,
+    display: 'flex'
+  },
 });
