722a58883577d66761a7c2b39a29e2bfd4ed1c78
luisfelipe2104 <gomesluisfelipe05@gmail.com>
2025-01-01 19:46:50 -0300

    adding navigation between screens

--- App.js
+++ App.js
@@ -1,141 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Vibration } from 'react-native';
-import { Audio } from 'expo-av';
 import React, { useState, useEffect, useContext } from 'react'
-import CircularProgress from 'react-native-circular-progress-indicator';
+import TimerScreen from './screens/TimerScreen/Index';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ConfigScreen from './screens/ConfigScreen/Index';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const [count, setCount] = useState(0)
-  const [defaultWorkMinutes, setDefaultWorkMinutes] = useState(25)
-  const [defaultPauseMinutes, setDefaultPauseMinutes] = useState(5)
-  const [minutes, setMinutes] = useState(defaultWorkMinutes)
-  const [seconds, setSeconds] = useState(0)
-  const [timePercent, setTimePercent] = useState(100)
-  const [time, setTime] = useState(`${defaultWorkMinutes}:00`)
-  const [isPaused, setIsPaused] = useState(false)
-  const [isStopped, setIsStopped] = useState(true)
-
-  const playBeep = async () => {
-    try {
-      const { sound } = await Audio.Sound.createAsync(
-        require('./assets/beep.mp3') // Update the path to your beep sound file
-      );
-      await sound.playAsync();
-      // Unload the sound after playback to free resources
-      sound.setOnPlaybackStatusUpdate((status) => {
-        if (status.didJustFinish) {
-          sound.unloadAsync();
-        }
-      });
-    } catch (error) {
-      console.error('Error playing sound:', error);
-    }
-  }
-
-  const timeIsOver = () => {
-    // gambiarra
-    setIsPaused(!isPaused)
-    Vibration.vibrate(3000)
-    playBeep()
-    if (isPaused) {
-      setMinutes(defaultWorkMinutes)
-    }
-    else if (!isPaused) {
-      setMinutes(defaultPauseMinutes)
-    }
-
-  }
-
-  const formatTime = () => {
-    const result = `${minutes < 10 ? "0" + minutes : minutes }:${seconds < 10 ? "0" + seconds : seconds}`
-    setTime(result)
-    const workMinutesPercent = (((minutes * 60) + seconds) * 100) / (isPaused === false ? 60 * defaultWorkMinutes : 60 * defaultPauseMinutes)
-    setTimePercent(workMinutesPercent.toFixed(2)) // alternative Math.floor()
-    return result
-  }
-
-  const buttonPress = () => {
-    setIsStopped(!isStopped)
-    Vibration.vibrate(100)
-  }
-
-  const tick = () => {
-    if (minutes === 0 & seconds === 0) {
-      timeIsOver()
-    }
-    else if (seconds === 0) {
-      setSeconds(59)
-      setMinutes(minutes - 1)
-    } 
-    else {
-      setSeconds(seconds - 1)
-    }
-  }
-
-  useEffect(() => {
-    if (!isStopped) {
-      const intervalId = setInterval(() => {
-        setCount(count + 1)
-      }, 1000)
-      console.log(count);
-      console.log(isPaused);
-      console.log(timePercent);
-      tick()
-      formatTime()
-      return () => clearInterval(intervalId);
-    }
-  }, [count, isStopped])
+  
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => buttonPress()} style={styles.button}>
-        <CircularProgress
-          radius={120}
-          value={timePercent}
-          titleColor={isStopped ? '#ad1a1f' :'#1ab85e'}
-          subtitleColor={isStopped ? '#78191c' : '#10572f'}
-          inActiveStrokeWidth={40}
-          activeStrokeWidth={20}
-          showProgressValue={false}
-          subtitle={isStopped === true ? "Start" : "Pause"}
-          subtitleStyle={styles.subtitle}
-          title={time}
-          titleStyle={styles.title}
-          inActiveStrokeColor={isStopped ? 'red' : '#2465FD'}
-          inActiveStrokeOpacity={0.2}
-          activeStrokeColor={isStopped ? '#d9141a': '#2465FD'}
-          circleBackgroundColor='rgba(30, 33, 63, 1)'
-          activeStrokeSecondaryColor={isStopped ? '#a63a3e' : '#C25AFF'}
-        />
-      </TouchableOpacity>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Timer" options={{headerShown: false}} component={TimerScreen} />
+        <Stack.Screen name="Config" options={{headerShown: false}} component={ConfigScreen} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1E213F',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: '#1E213F'
-  },
-  title: {
-    fontSize: 45,
-    display: 'flex',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    display: 'flex',
-    justifyContent: 'center'
-  },
-  subtitle: {
-    fontSize: 25,
-    display: 'flex',
-    fontStyle: 'italic',
-    textAlign: 'center',
-    display: 'flex',
-    justifyContent: 'center'
-  },
-});
---
+++ screens/ConfigScreen/Index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function ConfigScreen() {
+    return ( 
+        <View>
+            <Text>Config Screen</Text>
+        </View>  // or return your component here
+     );
+}
+
+export default ConfigScreen;
